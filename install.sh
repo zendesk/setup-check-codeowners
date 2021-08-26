dcdb7ade959fb6002a9db8708e4f37c74d408414
Rachel Evans <revans@zendesk.com>
2021-08-26 15:49:00 +0200

    Use GITHUB_ACTION_PATH

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
-install -d -m 755 ~/bin
-install -m 755 ./bin/check-codeowners ~/bin/
+install -d -m 755 "$HOME/bin/"
+install -m 755 "$GITHUB_ACTION_PATH/bin/check-codeowners" "$HOME/bin/"
 echo "$HOME/bin" >> "$GITHUB_PATH"
