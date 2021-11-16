503844b5fe15a8c27c29cdd0273cf87b9a463ea3
Benjamin Quorning <benjamin@quorning.net>
2021-11-16 12:48:51 +0100

    Perform `check_individual_patterns` in parallel
    On my machine (with 12 cores), running `check-codeowners --who-owns` on
    a number of files in a large repository (zendesk/zendesk) ran for more
    than 70 seconds before, compared to just 10 seconds now.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org" do
+  gem 'parallel'
   gem 'rspec'
 end
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+gem install parallel
 install -d -m 755 "$HOME/bin/"
 install -m 755 "$GITHUB_ACTION_PATH/bin/check-codeowners" "$HOME/bin/"
 echo "$HOME/bin" >> "$GITHUB_PATH"
