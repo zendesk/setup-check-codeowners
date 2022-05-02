7b71c0f86fd684ec6cdbddd280a3a92857519f32
Rachel Evans <revans@zendesk.com>
2022-05-02 08:15:19 +0200

    Rename 'r' to 'check_results'

--- src/lib/check_codeowners/check_runner.rb
+++ src/lib/check_codeowners/check_runner.rb
@@ -6,20 +6,20 @@ module CheckCodeowners
     end
 
     def show_checks
-      r = Checks.new(repo, options).results
+      check_results = Checks.new(repo, options).results
 
       if options.strict
-        r.errors.concat(r.warnings)
-        r.warnings = []
+        check_results.errors.concat(check_results.warnings)
+        check_results.warnings = []
       end
 
       if options.show_json
-        show_checks_json(r)
+        show_checks_json(check_results)
       else
-        show_checks_text(r)
+        show_checks_text(check_results)
       end
 
-      @has_errors = r.errors.any?
+      @has_errors = check_results.errors.any?
     end
 
     def errors?
@@ -30,18 +30,18 @@ module CheckCodeowners
 
     attr_reader :repo, :options
 
-    def show_checks_json(r)
+    def show_checks_json(check_results)
       output = {
-        errors: r.errors,
-        warnings: r.warnings
+        errors: check_results.errors,
+        warnings: check_results.warnings
       }
 
       if options.debug
         output.merge!(
           entries: report.codeowners.entries,
           owner_entries: report.codeowners.owner_entries,
-          all_files: r.all_files,
-          match_map: r.match_map,
+          all_files: check_results.all_files,
+          match_map: check_results.match_map,
         )
       end
 
@@ -49,11 +49,11 @@ module CheckCodeowners
       puts JSON.pretty_generate(output)
     end
 
-    def show_checks_text(r)
-      r.errors.each { |item| puts "ERROR: #{item[:message]}" }
-      r.warnings.each { |item| puts "WARNING: #{item[:message]}" }
+    def show_checks_text(check_results)
+      check_results.errors.each { |item| puts "ERROR: #{item[:message]}" }
+      check_results.warnings.each { |item| puts "WARNING: #{item[:message]}" }
 
-      if r.errors.any? || r.warnings.any?
+      if check_results.errors.any? || check_results.warnings.any?
         puts "For help, see https://github.com/zendesk/setup-check-codeowners/blob/main/Usage.md"
       end
     end
